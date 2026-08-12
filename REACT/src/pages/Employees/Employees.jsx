7b2aad3283c584c7e8e4a5941f13b9d8ab26299f
abhin-palakkal <abhinpalakkal24@gmail.com>
2026-08-13 00:49:46 +0530

    fix: attach jwt token to axios requests

--- REACT/src/pages/Employees/Employees.jsx
+++ REACT/src/pages/Employees/Employees.jsx
@@ -1,15 +1,63 @@
-//import
 import './Employees.css';
 import DashboardLayout from '../../../components/DashboardLayout/DashboardLayout';
+import { Table } from 'antd';
+import { useEffect, useState } from 'react';
+import axios from '../../../Utils/axios';
 
-const Employee = () => {
+const Employees = () => {
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    const response = await axios.get('/employee');
+    setData(response.data);
+  };
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Phone',
+      dataIndex: 'phone',
+      key: 'phone',
+    },
+    {
+      title: 'Department',
+      dataIndex: 'department',
+      key: 'department',
+    },
+    {
+      title: 'Position',
+      dataIndex: 'position',
+      key: 'position',
+    },
+    {
+      title: 'Salary',
+      dataIndex: 'salary',
+      key: 'salary',
+    },
+    {
+      title: 'Joining Date',
+      dataIndex: 'joiningDate',
+      key: 'joiningDate',
+    },
+  ];
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <>
-      <DashboardLayout>
-        <h1>Employee</h1>
-      </DashboardLayout>
-    </>
+    <DashboardLayout>
+      <h1>Employees</h1>
+      <Table columns={columns} dataSource={data} rowKey="_id" />
+    </DashboardLayout>
   );
 };
 
-export default Employee;
+export default Employees;
