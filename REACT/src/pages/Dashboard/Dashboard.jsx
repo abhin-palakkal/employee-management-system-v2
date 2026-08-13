cd69323a0571ae17b35bd01d91acc8726f1141f6
abhin-palakkal <abhinpalakkal24@gmail.com>
2026-08-13 15:53:41 +0530

    feat: add dashboard overview

--- REACT/src/pages/Dashboard/Dashboard.jsx
+++ REACT/src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,79 @@
 //import
 import './Dashboard.css';
 import DashboardLayout from '../../../components/DashboardLayout/DashboardLayout';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from '../../../Utils/axios';
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
+  const getData = async () => {
+    const response = await axios.get('/employee');
+    setData(response.data);
+  };
+  const salaryValue = data.reduce((accumulator, currentValue) => {
+    return accumulator + Number(currentValue.salary);
+  }, 0);
+  const currentYear = new Date().getFullYear();
+
+  const newEmployees = data.filter(employee => {
+    const joiningYear = new Date(employee.joiningDate).getFullYear();
+
+    return joiningYear === currentYear;
+  });
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <DashboardLayout>
-      <h1>Dashboard</h1>
+      <div className="dashboard-start">
+        <h1>Welcome back, Admin!</h1>
+        <p>Here's what's happening in your organization today.</p>
+      </div>
+
+      <div className="dashboard-center">
+        <div className="dashboard-card">
+          <h3>Total Employees</h3>
+          <h1>{data.length}</h1>
+          <p
+            onClick={() => {
+              navigate('/admin/employees');
+            }}
+          >
+            View all employees →
+          </p>
+        </div>
+
+        <div className="dashboard-card">
+          <h3>Departments</h3>
+          <h1>—</h1>
+          <p>Coming soon</p>
+        </div>
+
+        <div className="dashboard-card">
+          <h3>Total Salary</h3>
+          <h1>{salaryValue}</h1>
+          <p>View salary report →</p>
+        </div>
+
+        <div className="dashboard-card">
+          <h3>New Employees</h3>
+          <h1>{newEmployees.length}</h1>
+          <p>This Year</p>
+        </div>
+      </div>
+
+      <div className="dashboard-end">
+        <div className="recent-employees">
+          <h2>Recent Employees</h2>
+        </div>
+
+        <div className="department-summary">
+          <h2>Employees by Department</h2>
+        </div>
+      </div>
     </DashboardLayout>
   );
 };
